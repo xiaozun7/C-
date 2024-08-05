bd13aaad4daffcd42a3bb3080d45c5b74794b00d
25466 <25466@XIAOZUN>
2024-08-05 15:15:41 +0800

    添加项目文件。

---
+++ first/FileName.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//写代码 
+//1.写出主函数（main函数）
+//100-500 代码
+//如何执行呢？ - C语言是从主函数第一行开始执行的
+//所以C语言代码中得有main函数- 入口
+
+//写个代码，再屏幕上打印：小樽
+//printf - 库函数 - 再屏幕上打印信息的
+//printf 的使用，也得打招呼（引用头文件 stdio.h）
+
+#include <stdio.h>
+
+int main()
+{
+	printf("小樽\n");
+
+	return 0;
+}
